374897218c6a06b04aa819077f17865a006211fc
Ryan Johanson <ryanlindylive@gmail.com>
2019-03-28 12:42:55 -0700

    Fix punctuation in Most Popular Trip
    Refactor
    
    Punctuation was uneven so I fixed to be more consistent with rest of
    code.

--- bikesharecode.py
+++ bikesharecode.py
@@ -148,7 +148,7 @@ def time_stats(df):
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
-    print('\nCalculating The Most Popular Stations and Trip...\n')
+    print('\nCalculating the Most Popular Stations and Trip...\n')
     start_time = time.time()
 
     # display most commonly used start station
